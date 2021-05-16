b1f05e4318a0893974ff0ccb1074b76b57f22357
thomastam73 <thomastam73@gmail.com>
2021-05-17 01:49:51 +0800

    updated camera

--- app/src/main/java/com/example/assignment/fragment/LandMarkFragment.java
+++ app/src/main/java/com/example/assignment/fragment/LandMarkFragment.java
@@ -1,27 +1,150 @@
 package com.example.assignment.fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.graphics.Matrix;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.util.Rational;
+import android.util.Size;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.assignment.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.io.File;
+import java.util.Arrays;
+
+import static android.content.ContentValues.TAG;
 
 public class LandMarkFragment extends Fragment {
-
+    private int REQUEST_CODE_PERMISSIONS = 10;
+    private String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    TextureView textureView;
+    ImageButton button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_land_mark, container, false);
+        View root = inflater.inflate(R.layout.fragment_land_mark, container, false);
+        textureView = root.findViewById(R.id.viewFinder);
+        button = root.findViewById(R.id.camera_capture_button);
+        if (allPermissionGranted()) {
+            starCamera();
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+        return root;
+    }
+
+    private void starCamera() {
+        CameraX.unbindAll();
+        Rational aspectRatio = new Rational(textureView.getWidth(), textureView.getHeight());
+        Size screen = new Size(textureView.getWidth(), textureView.getHeight());
+        PreviewConfig previewConfig = new PreviewConfig.Builder().setTargetAspectRatio(aspectRatio).setTargetResolution(screen).build();
+        Preview preview = new Preview(previewConfig);
+        preview.setOnPreviewOutputUpdateListener(new Preview.OnPreviewOutputUpdateListener() {
+            @Override
+            public void onUpdated(Preview.PreviewOutput output) {
+                ViewGroup viewGroup = (ViewGroup) textureView.getParent();
+                viewGroup.removeView(textureView);
+                viewGroup.addView(textureView);
+                textureView.setSurfaceTexture(output.getSurfaceTexture());
+                updateTransform();
+            }
+        });
+        ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY).
+                setTargetRotation(getActivity().getWindowManager().getDefaultDisplay().getRotation()).build();
+        final ImageCapture imageCapture = new ImageCapture(imageCaptureConfig);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = new File("sdcard/photos/DCIM(0)/Camera/CameraX_" + System.currentTimeMillis());
+                imageCapture.takePicture(file, new ImageCapture.OnImageSavedListener() {
+                    @Override
+                    public void onImageSaved(@NonNull File file) {
+                        Log.d("ASDSD","good");
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
+                        if (cause != null) {
+                            cause.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+        CameraX.bindToLifecycle(this,preview,imageCapture);
+    }
+
+    private void updateTransform() {
+        Matrix matrix = new Matrix();
+        float w = textureView.getMeasuredWidth();
+        float h = textureView.getMeasuredHeight();
+        float cx = w / 2f;
+        float cy = h / 2f;
+        int rotationDgr;
+        int rotation = (int) textureView.getRotation();
+
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                rotationDgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotationDgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotationDgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotationDgr = 270;
+                break;
+            default:
+                return;
+        }
+        matrix.postRotate((float) rotationDgr, cx, cy);
+        textureView.setTransform(matrix);
+    }
+
+    private boolean allPermissionGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(getContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
