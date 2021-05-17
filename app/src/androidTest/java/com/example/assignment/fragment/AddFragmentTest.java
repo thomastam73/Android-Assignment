6818b181762dcc28c7db745efa1ae32ebf2935ce
thomastam73 <thomastam73@gmail.com>
2021-05-17 21:26:55 +0800

    testing

--- app/src/androidTest/java/com/example/assignment/fragment/AddFragmentTest.java
+++ app/src/androidTest/java/com/example/assignment/fragment/AddFragmentTest.java
@@ -1,19 +1,42 @@
 package com.example.assignment.fragment;
 
-import android.app.Activity;
+import android.widget.RelativeLayout;
 
-import junit.framework.TestCase;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.rule.ActivityTestRule;
 
+import com.example.assignment.R;
+import com.example.assignment.test.Test;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 
-public class AddFragmentTest extends TestCase {
+import static org.junit.Assert.*;
+
+public class AddFragmentTest {
     @Rule
+    public ActivityTestRule<Test> activityScenarioRule = new ActivityTestRule<Test>(Test.class);
 
+    private Test test = null;
+
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+        test = activityScenarioRule.getActivity();
     }
 
+    @org.junit.Test
+    public void testLaunch() {
+        RelativeLayout relativeLayout = (RelativeLayout) test.findViewById(R.id.test_container);
+        assertNotNull(relativeLayout);
+        AddFragment fragment = new AddFragment();
+        test.getFragmentManager().beginTransaction().add(relativeLayout.getId(),fragment).commitAllowingStateLoss();
+
+
+    }
+
+    @After
     public void tearDown() throws Exception {
+        test = null;
     }
-
 }
